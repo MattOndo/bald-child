afc754abdaa3ddfd58535d8b14fe87dd2cc3f313
Matt Ondo <yours.truly@mattondo.com>
2023-10-14 15:58:10 -0600

    use local highlightjs files

---
+++ includes/blocks/code/block.js
@@ -0,0 +1,3 @@
+window.addEventListener('load', () => {
+  hljs.highlightAll();
+});
--- includes/blocks/code/render.php
+++ includes/blocks/code/render.php
@@ -15,17 +15,32 @@ $block_id = $args['block_id'];
 
 // The block class names
 $class_name = $args['class_name'];
+
+if ($data['language'] === 'html') {
+    $languageClassname = 'xml';
+} else if ($data['language'] === 'command_line') {
+    $languageClassname = 'bash';
+} else {
+    $languageClassname = $data['language'];
+}
+
 ?>
 
 <!-- Our front-end template -->
 <div id="<?php echo $block_id; ?>" class="<?php echo $class_name; ?>">
+<small className='rounded-md opacity-50 p-0 pointer-events-none text-xs'><span className='hidden'>Language: </span>{label}</small>
+
     <?php
         if ( $data['language']) {
-            echo "<p>Language:" . $data['language'] . "</p>";
+            echo "<small><span>Language:</span>" . $data['language'] . "</small>";
+        } else {
+            echo "<p><strong>Select a language</strong></p>";
         }
 
         if ( $data['code']) {
-            echo "<code>" . html_entity_decode($data['code']) . "</code>";
+            echo "<pre><code class='language-" . $languageClassname . "'>" . htmlspecialchars($data['code']) . "</code></pre>";
+        } else {
+            echo "<p><strong>Enter some code</strong></p>";
         }
     ?>
 </div>
